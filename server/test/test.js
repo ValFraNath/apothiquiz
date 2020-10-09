4cb7610ee0b2de8a1a8af3cf9b082be11bf0983a
Nathanaël <contact@nathanaelhoun.fr>
2020-10-09 17:52:50 +0200

    Fix tests

--- server/test/test.js
+++ server/test/test.js
@@ -3,6 +3,6 @@ import app from "../index.js";
 
 describe("GET /", function() {
     it("responds with Hello World", function(done) {
-        request(app).get("/").expect('"Hello World !"', done);
+        request(app).get("/").expect('"Hello World!"', done);
     });
 });
