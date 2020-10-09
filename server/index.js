142685831f7667da2c55b528cfa5b38640d52950
Valentin Perignon <valentin.perignon@orange.fr>
2020-10-09 17:09:31 +0200

    Add default test (100% coverage) (#11)

--- server/index.js
+++ server/index.js
@@ -7,6 +7,4 @@ app.get("/",(req,res) => res.status(200).json("Hello World !"));
 
 app.listen(5035, () => console.log("Server is running on port 5035."));
 
-
-//export default app;
-
+export default app;
