2908c5ce468cb4313029ad0cbcb5b9b0de67ebd5
Nathanaël <contact@nathanaelhoun.fr>
2020-10-09 15:53:20 +0200

    Add CI/CD (#12)
    * Create main.yml
    
    * Add CI for PR
    
    * Update node.js.yml
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update index.js
    
    * Renamed workflow

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/",(req,res) => res.status(200).json("Hello World !"));
 
-app.listen(3000, () => console.log("Server is running on port 3000."));
+app.listen(5035, () => console.log("Server is running on port 5035."));
 
 
 //export default app;
