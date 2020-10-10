c48c5cf13748bb44d82684850a08071090de71ea
Valentin Perignon <valentin.perignon@orange.fr>
2020-10-10 16:03:47 +0200

    Add ESLint and Prettier (#23)
    * Adds eslint to client/
    
    * Adds eslint to server/
    
    * Add prettier to client and server

--- server/index.js
+++ server/index.js
@@ -6,18 +6,17 @@ const PORT = process.env.PORT || 5035;
 
 const app = express();
 
-app.use('/static', express.static('../client/build/static'));
-app.get('/', function (req, res) {
-  res.sendFile('index.html', { 'root': '../client/build/' });
+app.use("/static", express.static("../client/build/static"));
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: "../client/build/" });
 });
 
-app.get('/:dir/:file', function (req, res) {
+app.get("/:dir/:file", function (req, res) {
   var dir = req.params.dir,
     file = req.params.file;
 
-  res.sendFile(dir + '/' + file, { 'root': '../client/build/' });
+  res.sendFile(dir + "/" + file, { root: "../client/build/" });
 });
-
 
 app.get("/api/v1/status", (req, res) => res.status(200).json("Hello World!"));
 
