f0a20f4d0607fba2f0d3499bec60d99b14307c71
François Poguet <francoispoguet@gmail.com>
2020-10-10 09:50:29 +0200

    Serve the react files by server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from 'path';
 
 dotenv.config();
-const PORT = process.env.PORT;
+const {PORT, BUILT_PATH, IS_PROD} = process.env;
 
 const app = express();
 
-app.get("/",(req,res) => res.status(200).json("Hello World!"));
+if(IS_PROD === 'true') {
+    app.use(express.static(BUILT_PATH));
+    app.get("/", function (req, res) {
+        return res.sendFile(path.join(BUILT_PATH, 'index.html'));
+    })
+}
+
+app.get("/api",function(req,res){
+    return res.status(200).json("Hello World!");
+});
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 
