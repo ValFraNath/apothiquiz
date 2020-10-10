3b615075c58ea04ffde8ef1e546c63717dfe4e80
François Poguet <59446609+FrancoisPog@users.noreply.github.com>
2020-10-10 16:09:17 +0200

    Update client & server tests (#27)
    * test updated
    
    * test updated

--- server/test/test.js
+++ server/test/test.js
@@ -1,8 +1,9 @@
 import request from "supertest";
 import app from "../index.js";
 
-describe("GET /", function () {
-  it("responds with Hello World", function (done) {
-    request(app).get("/").expect('"Hello World!"', done);
-  });
+describe("GET /", function() {
+    it("responds with Hello World", function(done) {
+        request(app).get("/api/v1/status").expect('"Hello World!"', done);
+    });
 });
+
