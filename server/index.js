b77b1d656ae6a57fda131b5649165247f38f15e4
Nathanaël <contact@nathanaelhoun.fr>
2020-10-10 09:26:34 +0200

    Serve client from server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,20 @@ const PORT = process.env.PORT || 5035;
 
 const app = express();
 
-app.get("/",(req,res) => res.status(200).json("Hello World!"));
+app.use('/static', express.static('../client/build/static'));
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { 'root': '../client/build/' });
+});
+
+app.get('/:dir/:file', function (req, res) {
+  var dir = req.params.dir,
+    file = req.params.file;
+
+  res.sendFile(dir + '/' + file, { 'root': '../client/build/' });
+});
+
+
+app.get("/api/v1/status", (req, res) => res.status(200).json("Hello World!"));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 
