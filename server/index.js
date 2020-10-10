9f30e318e64d64ffb7b28b62d897242572a42b2e
François Poguet <francoispoguet@gmail.com>
2020-10-11 01:25:43 +0200

    some changes to test the react routing

--- server/index.js
+++ server/index.js
@@ -6,8 +6,11 @@ const PORT = process.env.PORT || 5035;
 
 const app = express();
 
+app.get("/api/v1/status", (req, res) => res.status(200).json("Hello World!"));
+
 app.use("/static", express.static("../client/build/static"));
-app.get("/", function (req, res) {
+
+app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "../client/build/" });
 });
 
@@ -18,7 +21,6 @@ app.get("/:dir/:file", function (req, res) {
   res.sendFile(dir + "/" + file, { root: "../client/build/" });
 });
 
-app.get("/api/v1/status", (req, res) => res.status(200).json("Hello World!"));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 
