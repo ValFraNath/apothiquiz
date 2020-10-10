3c2fe2cc6cb277eb953787fe404556d3a2267251
François Poguet <francoispoguet@gmail.com>
2020-10-10 13:38:38 +0200

    test updated

--- server/test/test.js
+++ server/test/test.js
@@ -3,6 +3,6 @@ import app from "../index.js";
 
 describe("GET /", function() {
     it("responds with Hello World", function(done) {
-        request(app).get("/").expect('"Hello World!"', done);
+        request(app).get("/api/v1/status").expect('"Hello World!"', done);
     });
 });
