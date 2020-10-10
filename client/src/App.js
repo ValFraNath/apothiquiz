09574977cbee0fc2dd0b5382380dd3489ce99c24
François Poguet <francoispoguet@gmail.com>
2020-10-10 15:24:13 +0200

    put the link in a route

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import './App.css';
 import HelloWorld from "./components/HelloWorld";
 
-
 function App() {
     return (
         <Router>
-            <Link to="/hello_world">Go to Hello World</Link>
+            <Route path="/" exact >
+                <Link to="/hello_world">Go to Hello World</Link>
+            </Route>
             <Route path='/hello_world' component={HelloWorld}/>
         </Router>
     );
