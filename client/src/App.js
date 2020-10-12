ba1c6de26d766a63c3b741529ef902e3553a07f6
François Poguet <59446609+FrancoisPog@users.noreply.github.com>
2020-10-12 13:25:25 +0200

    Improve React router & server routes (#40)

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact>
-        <Link to="/hello_world">Go to Hello World now!</Link>
-      </Route>
-      <Route path="/hello_world" component={HelloWorld} />
+      <Switch>
+        <Route path="/" exact>
+          <Link to="/hello_world">Go to Hello World</Link>
+        </Route>
+        <Route path="/hello_world" exact component={HelloWorld} />
+        <Route status={404} >Not Found</Route>
+      </Switch>
     </Router>
   );
 }
