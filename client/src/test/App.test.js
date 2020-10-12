0a70733da6bfac3adfb2e4bf2e57d3ab15b3cf69
François Poguet <59446609+FrancoisPog@users.noreply.github.com>
2020-10-13 00:02:56 +0200

    Organize folders  & update client test (#46)
    * Styles reset added & folder organization
    
    * Update client tests ( jest & chai )

---
+++ client/src/test/App.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { expect } from "chai";
+import { render } from "@testing-library/react";
+import App from "../App";
+/* eslint-disable */
+describe("Good display", () => {
+  // Jest test
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+  });
+
+  // Chai test
+  it("Contains the good link", () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Go to Hello World/i)).to.be.not.null;
+  });
+});
