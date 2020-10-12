a1dc288743f97df51fb65d40f1a32905e7118df4
FrancoisPog <francoispoguet@gmail.com>
2020-10-12 15:47:44 +0200

    Creation of apiRouter & reactRouter

--- server/index.js
+++ server/index.js
@@ -1,20 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import apiRouter from "./routes/api.route.js";
+import reactRouter from "./routes/reactRouter.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5035;
 
 const app = express();
+app.use(express.static("../client/build/"));
 
 app.use("/api/v1/", apiRouter);
-app.use("/static", express.static("../client/build/static"));
-
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "../client/build/" });
-});
-
-
+app.use("/",reactRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 
---
+++ server/routes/reactRouter.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+const reactRouter = express.Router();
+
+function goToReactIndex (req, res) {
+    res.sendFile("index.html", { root: "../client/build/" });
+}
+
+reactRouter.get("/", goToReactIndex);
+
+reactRouter.get("/hello_world", goToReactIndex);
+
+
+export default reactRouter;
