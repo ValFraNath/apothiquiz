d92da1aae36c5fd80cf088bed3ae6e4dda18acae
François Poguet <francoispoguet@gmail.com>
2020-10-12 11:54:45 +0200

    Improve react router and server routes

--- server/index.js
+++ server/index.js
@@ -1,25 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import apiRouter from "./routes/api.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5035;
 
 const app = express();
 
-app.get("/api/v1/status", (req, res) => res.status(200).json("Hello World!"));
-
+app.use("/api/v1/", apiRouter);
 app.use("/static", express.static("../client/build/static"));
 
 app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "../client/build/" });
 });
 
-app.get("/:dir/:file", function (req, res) {
-  var dir = req.params.dir,
-    file = req.params.file;
 
-  res.sendFile(dir + "/" + file, { root: "../client/build/" });
-});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 
---
+++ server/routes/api.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const apiRouter = express.Router();
+
+apiRouter.get("/status", function(req, res) {
+ res.status(200).json("Hello World!");
+});
+
+export default apiRouter;
