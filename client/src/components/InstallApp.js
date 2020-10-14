86139cb783cd86c156a6fc993ba3f5a8dbce70f7
Valentin Perignon <valentin.perignon@orange.fr>
2020-10-14 21:36:11 +0200

    And then, the button

---
+++ client/src/components/InstallApp.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class InstallApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  handleClick = () => {
+    this.props.installPrompt.prompt();
+  }
+
+  render() {
+    return (
+      <div>
+        <button id="installButton" onClick={this.handleClick}>Install this app and become a medical expert!</button>
+      </div>
+    );
+  }
+}
