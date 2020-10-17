ba17c578c5ae1ebbdd5da223836ec476fa8ee37a
Nathanaël <contact@nathanaelhoun.fr>
2020-10-17 14:47:22 +0200

    Add tests

--- server/index.js
+++ server/index.js
@@ -1,32 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
-import apiRouter from "./routes/api.route.js";
-import mysql from "mysql";
 
+import apiRouter from "./routes/api.route.js";
 import reactRouter from "./routes/reactRouter.js";
+import db from "./db/database.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5035;
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "glowing-octo-guacamole",
-  password: process.env.DB_PASSWORD || "p@ssword",
-  database: process.env.DB_DATABASE || "glowingOctoGuacamole",
-});
 
 db.connect((err) => {
   if (err) {
-    console.log("Can't connect to the database.");
+    console.error("Can't connect to the database.");
     throw err;
   }
 
   console.log("Connected to database!");
 });
 
+const PORT = process.env.PORT || 5035;
 const app = express();
-app.use(express.static("../client/build/"));
 
+app.use(express.static("../client/build/"));
 app.use("/api/v1/", apiRouter);
 app.use("/", reactRouter);
 
