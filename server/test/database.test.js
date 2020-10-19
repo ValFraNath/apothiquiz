eaa2e1311fa730b350f42960fc34ee4ab4dfc77e
FrancoisPog <francoispoguet@gmail.com>
2020-10-19 17:47:53 +0200

    New tests for database structure

--- server/test/database.test.js
+++ server/test/database.test.js
@@ -45,3 +45,62 @@ describe("Create and delete table", function () {
     });
   });
 });
+
+describe("Check the database structure", function () {
+  let structure = [
+    {
+      name: "molecule",
+      fields: [
+        "mo_ID",
+        "mo_name",
+        "mo_dci",
+        "mo_difficulty",
+        "mo_skeletal_formula",
+      ],
+    },
+    {
+      name: "class",
+      fields: ["cl_id", "cl_name"],
+    },
+    {
+      name: "property",
+      fields: ["pr_id", "pr_name"],
+    },
+    {
+      name: "molecule_property",
+      fields: ["pr_id", "mo_id"],
+    },
+    {
+      name: "molecule_class",
+      fields: ["cl_id", "mo_id"],
+    },
+  ];
+
+  for (let table of structure) {
+    it(`Table '${table.name}' fields`, function (done) {
+      let sql = `SELECT *
+                    FROM INFORMATION_SCHEMA.COLUMNS
+                    WHERE TABLE_NAME = '${table.name}'`;
+
+      db.query(sql, function (err, res) {
+        if (err) throw err;
+        assert(res.length !== 0, `The table '${table.name}' doesn't exist. `);
+
+        let fieldsToTest = res.map((e) => e["COLUMN_NAME"]);
+        assert.strictEqual(
+          fieldsToTest.length,
+          table.fields.length,
+          "Incorrect number of fields."
+        );
+
+        table.fields.forEach((field) => {
+          assert(
+            fieldsToTest.includes(field),
+            `Incorrect field :  "${field}". `
+          );
+        });
+        done();
+      });
+    });
+  }
+});
