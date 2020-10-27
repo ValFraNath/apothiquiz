d4a56ae487a229fb2d3e779ab8c5db416f6e69ba
Valentin Perignon <valentin.perignon@orange.fr>
2020-10-27 21:02:16 +0100

    Initial display

--- client/src/components/Train.js
+++ client/src/components/Train.js
@@ -3,11 +3,68 @@ import React, { Component } from "react";
 class Train extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      question: {},
+      answers: [],
+      questionNumber: 0,
+      time: null
+    };
+  }
+
+  getNewQuestion = () => {
+    const newQuestion = {
+      type: 1,
+      subject: "heprès",
+      goodAnswer: "valganciclovir",
+      badAnswers: [
+        "zidovudine",
+        "efavirenz",
+        "lamotrigine"
+      ]
+    };
+
+    const newAnswers = [newQuestion.goodAnswer, ...newQuestion.badAnswers];
+
+    this.setState({
+      question: newQuestion,
+      questionNumber: this.state.questionNumber + 1,
+      answers: newAnswers,
+      time: 5
+    });
   }
 
   render() {
-    return (<p>Hello Everybody!</p>);
+    const { question, answers, questionNumber, time } = this.state;
+    const introductionView = (
+      <>
+        <h1>Mode entraînement</h1>
+        <p>Une série de questions</p>
+        <button onClick={this.getNewQuestion}>Lancer l'entraînement</button>
+      </>
+    );
+
+    return (
+      <div>
+        { this.state.questionNumber === 0 ?
+          introductionView :
+          <>
+            <h2>Question n°{questionNumber}</h2>
+            <h1>{question.subject}</h1>
+
+            <p id="question-timer">{time} s</p>
+
+            <div id="question-answers">
+              {
+                answers.map((value, index) => (
+                  <button key={index}>{value}</button>
+                ))
+              }
+            </div>
+
+          </>
+        }
+      </div>
+    );
   }
 }
 
