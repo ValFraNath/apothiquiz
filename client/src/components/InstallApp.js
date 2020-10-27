ada666bb4e0a3a652bc0da5572592d75f521fd08
Valentin Perignon <valentin.perignon@orange.fr>
2020-10-27 16:54:05 +0100

    Add PropType

--- client/src/components/InstallApp.js
+++ client/src/components/InstallApp.js
@@ -1,9 +1,16 @@
 import React from "react";
+import PropTypes from "proptypes";
 
 const InstallApp = ({ installPromptEvent }) => (
   <button id="installButton" onClick={() => installPromptEvent.prompt()}>
     Install Guacamole
   </button>
 );
+
+InstallApp.propTypes = {
+  installPromptEvent: PropTypes.shape({
+    prompt: PropTypes.func.isRequired,
+  }),
+};
 
 export default InstallApp;
