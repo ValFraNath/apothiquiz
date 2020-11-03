47e9df0cc4bdf2dcf3685c77452541b1e5c2401c
Nathanaël <contact@nathanaelhoun.fr>
2020-11-03 09:14:40 +0100

    Refactored client source

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import * as serviceWorker from "./serviceWorker";
 import { ReloadIcon } from "@modulz/radix-icons";
 
 import "./styles/styles.scss";
-import HelloWorld from "./components/HelloWorld";
-import InstallApp from "./components/InstallApp";
-import Train from "./components/Train";
-import OfflineBanner from "./components/OfflineBanner";
+import HelloWorld from "./components/system/HelloWorld";
+import InstallApp from "./components/system/InstallApp";
+import Train from "./components/layouts/Train";
+import OfflineBanner from "./components/system/OfflineBanner";
 
 export default class App extends Component {
   constructor(props) {
