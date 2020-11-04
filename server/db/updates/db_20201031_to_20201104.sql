fd7fc2d81b073e6da638be35fe19d5ae36ce388e
Francois Poguet <francoispoguet@gmail.com>
2020-11-04 21:37:36 +0100

    correct error

--- server/db/updates/db_20201031_to_20201104.sql
+++ server/db/updates/db_20201031_to_20201104.sql
@@ -1,20 +1,11 @@
 -- constraints table `molecule_class`
 ALTER TABLE `molecule_class`
-    DROP CONSTRAINT `molecule_class_ibfk_1`;
-ALTER TABLE `molecule_class`
-    DROP CONSTRAINT `molecule_class_ibfk_2`;
-
-ALTER TABLE `molecule_class`
-    ADD CONSTRAINT `molecule_class_ibfk_1` FOREIGN KEY (`mo_id`) REFERENCES `molecule` (`mo_id`) ON UPDATE CASCADE ON DELETE RESTRICT ,
-    ADD CONSTRAINT `molecule_class_ibfk_2` FOREIGN KEY (`cl_id`) REFERENCES `class` (`cl_id`) ON UPDATE CASCADE ON DELETE RESTRICT ;
+    ADD CONSTRAINT `molecule_class_ibfk_3` FOREIGN KEY (`mo_id`) REFERENCES `molecule` (`mo_id`) ON UPDATE CASCADE ON DELETE RESTRICT ,
+    ADD CONSTRAINT `molecule_class_ibfk_4` FOREIGN KEY (`cl_id`) REFERENCES `class` (`cl_id`) ON UPDATE CASCADE ON DELETE RESTRICT ;
 
 -- Contstraints table `molecule_property`
 ALTER TABLE `molecule_property`
-    DROP CONSTRAINT `molecule_property_ibfk_1`,
-    DROP CONSTRAINT `molecule_property_ibfk_2`;
-
-ALTER TABLE `molecule_property`
-    ADD CONSTRAINT `molecule_property_ibfk_1` FOREIGN KEY (`mo_id`) REFERENCES `molecule` (`mo_id`) ON UPDATE CASCADE ON DELETE RESTRICT ,
-    ADD CONSTRAINT `molecule_property_ibfk_2` FOREIGN KEY (`pr_id`) REFERENCES `property` (`pr_id`) ON UPDATE CASCADE ON DELETE RESTRICT ;
+    ADD CONSTRAINT `molecule_property_ibfk_3` FOREIGN KEY (`mo_id`) REFERENCES `molecule` (`mo_id`) ON UPDATE CASCADE ON DELETE RESTRICT ,
+    ADD CONSTRAINT `molecule_property_ibfk_4` FOREIGN KEY (`pr_id`) REFERENCES `property` (`pr_id`) ON UPDATE CASCADE ON DELETE RESTRICT ;
 
 UPDATE `system` SET `sy_version` = "2020-11-04";
