88b7d9a2a964be01321adaff812183ee135ce8c7
François Poguet <59446609+FrancoisPog@users.noreply.github.com>
2021-01-11 13:47:06 +0100

    The database gets a new look  (#107)
    * Update the database with a new creation script
    
    * Refactor system information table, fix strings in corresponding client UI
    
    Co-authored-by: Nathanaël Houn <contact@nathanaelhoun.fr>

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 
-import { status } from "../controllers/api.js";
-import { generateQuestion } from "../controllers/question.js";
+import ApiController from "../controllers/api.js";
+import QuestionController from "../controllers/question.js";
 import UserController from "../controllers/user.js";
 import auth from "../middlewares/auth.middleware.js";
 
 const apiRouter = express.Router();
 
-apiRouter.get("/status", status);
+apiRouter.get("/status", ApiController.status);
 
-apiRouter.get("/question/:type", generateQuestion);
+apiRouter.get("/question/:type", QuestionController.generateQuestion);
 
 apiRouter.get("/user", auth, UserController.getInfos);
 
