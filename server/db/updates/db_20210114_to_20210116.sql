d3768e38facb289b32bbcceeac33ad51a7ff8f4b
Francois Poguet <francoispoguet@gmail.com>
2021-01-16 13:06:24 +0100

    remove useless database name in command

--- server/db/updates/db_20210114_to_20210116.sql
+++ server/db/updates/db_20210114_to_20210116.sql
@@ -1,13 +1,13 @@
 RENAME TABLE `property` TO `property_value`;
 RENAME TABLE `category` TO `property`;
 
-ALTER TABLE `glowingoctoguacamole`.`property_value` DROP FOREIGN KEY `property_value_ibfk_1`;
+ALTER TABLE `property_value` DROP FOREIGN KEY `property_value_ibfk_1`;
 
 ALTER TABLE `property` CHANGE `ca_id` `ca_id` INT(11) NOT NULL; 
 ALTER TABLE `property` DROP PRIMARY KEY ;
 ALTER TABLE `property` CHANGE `ca_id` `pr_id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, CHANGE `ca_name` `pr_name` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL; 
 
-ALTER TABLE `glowingoctoguacamole`.`molecule_property` DROP FOREIGN KEY `molecule_property_ibfk_2`;
+ALTER TABLE `molecule_property` DROP FOREIGN KEY `molecule_property_ibfk_2`;
 
 ALTER TABLE `property_value` CHANGE `pr_id` `pr_id` INT(11) NOT NULL; 
 ALTER TABLE `property_value` DROP PRIMARY KEY ;
