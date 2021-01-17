141d5c27d3682805223587907bfb2ee8fc60c7c9
Valentin Perignon <valentin.perignon@orange.fr>
2021-01-18 00:04:32 +0100

    Initial file

---
+++ client/src/components/pages/UserHomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class UserHomePage extends Component {
+  render() {
+    return (
+      <main id="userhomepage">
+        <p>Welcome</p>
+      </main>
+    );
+  }
+}
+
+export default UserHomePage;
