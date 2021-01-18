ea04b568894739701ff7e90f2fb02075793e9d9d
François Poguet <59446609+FrancoisPog@users.noreply.github.com>
2021-01-18 11:55:20 +0100

    Update server/test/data_importer/data_importer.test.js
    Co-authored-by: Valentin Perignon <valentin.perignon@orange.fr>

--- server/test/data_importer/data_importer.test.js
+++ server/test/data_importer/data_importer.test.js
@@ -128,7 +128,7 @@ async function getMoleculeData(dci) {
  */
 async function getClassification(classification) {
   let singular = classification.replace(/e?s$/, "");
-  let prefix = classification.slice(0, 2);
+  const prefix = classification.slice(0, 2);
   const sql = `SELECT l3.${prefix}_name as ${singular}, l2.${prefix}_name as parent, l1.${prefix}_name as grand_parent 
               FROM ((${singular} as l1 RIGHT OUTER JOIN ${singular} as l2 ON l1.${prefix}_id = l2.${prefix}_higher) 
                 RIGHT OUTER JOIN ${singular} as l3 ON l3.${prefix}_higher = l2.${prefix}_id) `;
