035f2aa378ce3839dfadbfdf87774b159c353918
Francois Poguet <francoispoguet@gmail.com>
2021-01-21 15:09:30 +0100

    add procedure & tests

--- server/db/updates/db_20210116_to_20210121.sql
+++ server/db/updates/db_20210116_to_20210121.sql
@@ -13,6 +13,27 @@ SELECT *
 FROM classification 
 ORDER BY cl_level DESC;
 
+CREATE PROCEDURE `getSystemsOf`(IN `dci` VARCHAR(256)) NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER 
+WITH RECURSIVE classification AS (
+    SELECT sy_id ,sy_name ,sy_higher ,sy_level 
+    FROM system JOIN molecule ON mo_system = sy_id 
+    WHERE molecule.mo_dci = dci 
+    
+    UNION ALL 
+    
+    SELECT S.sy_id ,S.sy_name ,S.sy_higher ,S.sy_level 
+    FROM system S JOIN classification child ON S.sy_id = child.sy_higher
+) 
+SELECT * 
+FROM classification 
+ORDER BY sy_level DESC;
+
+CREATE PROCEDURE `getPropertyValuesOf`(IN `dci` VARCHAR(256), IN `propertyName` VARCHAR(256)) NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER 
+SELECT mo_dci as molecule, pv_name as value 
+FROM molecule NATURAL JOIN molecule_property NATURAL JOIN property_value JOIN property ON pv_property = pr_id 
+WHERE mo_dci = dci 
+AND pr_name = propertyName;
+
 
 UPDATE `server_informations`
     SET `value` = "2021-01-21" WHERE `key` = "api_version";
--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,3 +1,7 @@
+import fs from "fs/promises";
+import path from "path";
+
+import { queryPromise } from "../db/database.js";
 import app from "../index.js";
 
 /**
@@ -15,4 +19,12 @@ export function forceTruncateTables(...tables) {
   let sql = "SET FOREIGN_KEY_CHECKS = 0; ";
   sql = tables.reduce((sql, table) => sql + `TRUNCATE TABLE ${table} ; `, sql);
   return sql + "SET FOREIGN_KEY_CHECKS = 1; ";
+}
+
+export function insertData(filename) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.resolve("test", "required_data", filename), { encoding: "utf8" })
+      .then((script) => queryPromise(script).then(() => resolve()))
+      .catch(reject);
+  });
 }
