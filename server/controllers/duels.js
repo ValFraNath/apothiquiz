0dc9103150871cd67fda9d231f2b118e11223a46
Francois Poguet <francoispoguet@gmail.com>
2021-01-23 10:27:06 +0100

    Handle syntax error in request

---
+++ server/controllers/duels.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+function create(req, res) {}
+
+// eslint-disable-next-line no-unused-vars
+function fetch(req, res) {}
+
+// eslint-disable-next-line no-unused-vars
+function play(req, res) {}
+
+export default { create, fetch, play };
--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -3,6 +3,7 @@ import express from "express";
 import ApiController from "../controllers/api.js";
 import QuestionController from "../controllers/question.js";
 import UserController from "../controllers/user.js";
+import DuelController from "../controllers/duels.js";
 import auth from "../middlewares/auth.middleware.js";
 
 const apiRouter = express.Router();
@@ -16,5 +17,11 @@ apiRouter.post("/user/login", UserController.login);
 apiRouter.get("/user/:pseudo", auth, UserController.getInfos);
 
 apiRouter.patch("/user/:pseudo", auth, UserController.saveInfos);
+
+apiRouter.post("/duel/new", auth, DuelController.create);
+
+apiRouter.get("/duel/:id", auth, DuelController.fetch);
+
+apiRouter.post("/duel/:id/:round", auth, DuelController.play);
 
 export default apiRouter;
