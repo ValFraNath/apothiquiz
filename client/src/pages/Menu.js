86fc7807b03bca524e6a2204c5eec31ea3052303
Valentin Perignon <valentin.perignon@orange.fr>
2021-01-28 00:03:35 +0100

    Replace location

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AuthService from "../services/auth.service";
 
 import InstallApp from "../components/system/InstallApp";
 
 const Menu = ({ installPromptEvent, user }) => {
+  if (AuthService.getCurrentUser() !== null) {
+    document.location.replace("/homepage");
+  }
+
   return (
     <main id="menu">
       <div id="list">
