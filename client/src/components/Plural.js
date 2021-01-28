4b7b3fb053ff0459bccb8078f98e2de527dd1c67
Valentin Perignon <valentin.perignon@orange.fr>
2021-01-28 23:44:18 +0100

    Update tests

--- client/src/components/Plural.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Plural = ({ word, count, plural = null }) => {
-  const pluralWord = plural ? plural : `${word}s`;
-  return <>{count <= 1 ? word : pluralWord}</>;
-};
-
-export default Plural;
--- server/test/required_data/users.sql
+++
@@ -1,6 +0,0 @@
-INSERT IGNORE INTO `user` (`us_login`, `us_avatar`)
-VALUES
-('fpoguet','{"eyes":0,"hands":0,"hat":0,"mouth":0,"colorBody":"#0c04fc","colorBG":"#D3D3D3"}'),
-('vperigno','{"eyes":0,"hands":0,"hat":0,"mouth":0,"colorBody":"#0c04fc","colorBG":"#D3D3D3"}'),
-('nhoun', '{"eyes":0,"hands":0,"hat":0,"mouth":0,"colorBody":"#0c04fc","colorBG":"#D3D3D3"}'),
-('test', '{"eyes":0,"hands":0,"hat":0,"mouth":0,"colorBody":"#0c04fc","colorBG":"#D3D3D3"}');
