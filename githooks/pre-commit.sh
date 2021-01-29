eef7937eceab1213dfa83a35cbb52784a8f31c50
Nathanaël <contact@nathanaelhoun.fr>
2021-01-29 13:53:04 +0100

    Add format check to ci (#164)
    * Add format check to CI
    * Use separate steps for each command
    * Optimize steps order
    * Rename workflows
    * Explicitly use ubuntu 20.04

--- githooks/pre-commit.sh
+++ githooks/pre-commit.sh
@@ -2,15 +2,15 @@
 
 if [ "$(git diff --name-only HEAD | grep "^server/")" != "" ]; then
     cd "$(git rev-parse --show-toplevel)/server" || exit 1
-    npm run format || exit 1
-    npm run lint || exit 1
+    npm run format:write || exit 1
+    npm run lint:fix || exit 1
     npm run test || exit 1
 fi
 
 if [ "$(git diff --name-only HEAD | grep "^client/")" != "" ]; then
     cd "$(git rev-parse --show-toplevel)/client" || exit 1
-    npm run format || exit 1
-    npm run lint || exit 1
+    npm run format:write || exit 1
+    npm run lint:fix || exit 1
     npm run test -- --watchAll=false || exit 1
 fi
 
