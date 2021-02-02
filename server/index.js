e77875b00920f1461b7620437a552c3862919184
Francois Poguet <francoispoguet@gmail.com>
2021-02-02 21:00:18 +0100

    change in index too

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import express from "express";
 
 import Database from "./db/database.js";
 import Logger from "./global/Logger.js";
-import auth from "./middlewares/auth.middleware.js";
+import AuthenticationMiddleware from "./middlewares/auth.middleware.js";
 import RequestSyntaxErrorHandler from "./middlewares/error.middleware.js";
 import apiRouter from "./routes/api.route.js";
 import reactRouter from "./routes/react.route.js";
@@ -31,16 +31,9 @@ app.use("/api/v1/", apiRouter);
 app.use("/", reactRouter);
 app.use(RequestSyntaxErrorHandler);
 
-try {
-  fs.mkdirSync("files");
-} catch (error) {
-  if (error.code !== "EEXIST") {
-    Logger.error(error, "Can't create the 'files' directory");
-    process.exit(1);
-  }
-}
+fs.mkdirSync("files", { recursive: true });
 
-app.use(auth, express.static("files"));
+app.use(AuthenticationMiddleware, express.static("files"));
 
 Database.connect()
   .then(() => {
