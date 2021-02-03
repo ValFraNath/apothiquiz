b22b094ad60e3f0a10c0ae839e8177ccc4f5060c
Francois Poguet <francoispoguet@gmail.com>
2021-02-03 18:17:41 +0100

    first

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Admin extends Component {
+  // constructor() {
+  //   super();
+  // }
+
+  render() {
+    return (
+      <main id="administration">
+        <h1>Espace Administration</h1>
+      </main>
+    );
+  }
+}
+
+// const FileImporter = () => {};
