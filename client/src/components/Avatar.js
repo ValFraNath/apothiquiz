7f8cd16cea5dbd5e3ced7156a1dd0226b97b2a74
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-02-11 17:36:46 +0100

    Better avatar component

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,40 +1,28 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-import { ReactComponent as BodyBottom } from "../images/avatar/bodyBottom.svg";
+import { ReactComponent as BodyBottomImage } from "../images/avatar/bodyBottom.svg";
 import ImageBodyTop from "../images/avatar/bodyTop.png";
 import ImageEyes from "../images/avatar/eyes.png";
 import ImageHands from "../images/avatar/hands.png";
 import ImageHats from "../images/avatar/hats.png";
 import ImageMouthes from "../images/avatar/mouthes.png";
 
-const Avatar = function ({
-  size,
-  infos: { eyes, hands, hat, mouth, colorBG, colorBody },
-  reversed,
-}) {
-  return (
+const Avatar = ({ size, infos: { eyes, hands, hat, mouth, colorBG, colorBody }, reversed }) => (
+  <div className="avatar" style={{ width: size, height: size }}>
     <div
-      className="avatar"
-      style={{
-        width: size,
-        height: size,
-      }}
+      className="avatar-inside"
+      style={{ backgroundColor: colorBG, transform: reversed ? "rotateY(180deg)" : "" }}
     >
-      <div
-        className="avatar-inside"
-        style={{ backgroundColor: colorBG, transform: reversed ? "rotateY(180deg)" : "" }}
-      >
-        <img className="avatar-eyes" src={ImageEyes} data-choice={eyes} alt="Avatar eyes" />
-        <img className="avatar-hands" src={ImageHands} data-choice={hands} alt="Avatar hands" />
-        <img className="avatar-hat" src={ImageHats} data-choice={hat} alt="Avatar hat" />
-        <img className="avatar-mouth" src={ImageMouthes} data-choice={mouth} alt="Avatar mouth" />
-        <img className="avatar-body-top" src={ImageBodyTop} alt="Avatar body" />
-        <BodyBottom className="avatar-body-bottom" fill={colorBody} alt="Avatar body" />
-      </div>
+      <img className="avatar-eyes" src={ImageEyes} data-choice={eyes} alt="Avatar eyes" />
+      <img className="avatar-hands" src={ImageHands} data-choice={hands} alt="Avatar hands" />
+      <img className="avatar-hat" src={ImageHats} data-choice={hat} alt="Avatar hat" />
+      <img className="avatar-mouth" src={ImageMouthes} data-choice={mouth} alt="Avatar mouth" />
+      <img className="avatar-body-top" src={ImageBodyTop} alt="Avatar body" />
+      <BodyBottomImage className="avatar-body-bottom" fill={colorBody} alt="Avatar body" />
     </div>
-  );
-};
+  </div>
+);
 
 Avatar.defaultProps = {
   infos: {
