5a2ca7c2bd45e57a1eb5cc5a0763c9e74479ef09
Francois Poguet <francoispoguet@gmail.com>
2021-02-11 17:30:07 +0100

    first filename checks

--- server/global/ImageFilesAnalyzer.js
+++ server/global/ImageFilesAnalyzer.js
@@ -1,7 +1,26 @@
-// /**
-//  *
-//  * @param {string[]} filenames
-//  */
-// function analyseImageFilenames(filenames) {
-//   return new Promise((resolve, reject) => {});
-// }
+/**
+ *
+ * @param {string[]} filenames
+ */
+function analyseImageFilenames(filenames) {
+  return new Promise((resolve, reject) => {
+    const normalizedFilenames = filenames.map(normalizeDCI);
+  });
+}
+
+/**
+ * Returns values that appear more than once in the list
+ * @param {any[]} values
+ * @returns {any[]} non-unique values
+ */
+function getDuplicates(values) {
+  return [...new Set(values.filter((value, i) => values.slice(i + 1).includes(value)))];
+}
+
+const normalizeDCI = (str) =>
+  str
+    .trim()
+    .normalize("NFD")
+    .toLowerCase()
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s+/, "_");
