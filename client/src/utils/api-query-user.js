fbf8cfc5e7c5979629c4d5c4dc30680b487b313c
Francois Poguet <francoispoguet@gmail.com>
2021-02-18 16:42:49 +0100

    change async/await to then/catch

--- client/src/utils/api-query-user.js
+++ client/src/utils/api-query-user.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
-async function getUserInfos(username) {
-  return (await axios.get(`/api/v1/users/${username}`)).data;
+/**
+ * Get a user informations
+ * @param {string} username The user name
+ * @returns {Promise<object>} The user informations
+ */
+function getUserInfos(username) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`/api/v1/users/${username}`)
+      .then((res) => resolve(res.data))
+      .catch(reject);
+  });
 }
 
 export { getUserInfos };
