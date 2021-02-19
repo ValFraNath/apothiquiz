9dbe26a0bd892aed4d62791a4e166fe986069c91
Francois Poguet <francoispoguet@gmail.com>
2021-02-19 10:10:32 +0100

    refactor

--- server/global/users_importation/usersAnalyzer.js
+++ server/global/users_importation/usersAnalyzer.js
@@ -2,51 +2,81 @@ import levenshtein from "js-levenshtein";
 
 import { LOGIN_MAX_LENGTH } from "./usersImporter.js";
 
-const LOGIN_MAX_DISTANCE = 2;
+const LOGIN_MAX_DISTANCE = 1;
 
+/**
+ * Analyze a list of users
+ * @param {{login,admin}[]} list The user list
+ * @returns {UsersAnalyzerWarning[]} The warnings list
+ */
 export function analyzeUsers(list) {
   const warnings = [];
   const loginList = list.map((user) => user.login);
-  const duplicatesLogin = getDuplicates(loginList);
 
-  warnings.push(...loginList.map(isLoginValid));
+  const duplicatesLogin = getDuplicates(loginList);
+  const tooClosesLogin = getTooCloseLogins(loginList);
+  const invalidLogins = getInvalidLogins(loginList);
+  const tooLongLogins = getTooLongLogins(loginList);
+
+  warnings.push(
+    ...duplicatesLogin.map(
+      (login) =>
+        new UsersAnalyzerWarning(
+          UsersAnalyzerWarning.DUPLICATE_LOGIN,
+          `Duplication du login "${login}"`
+        )
+    )
+  );
+
+  warnings.push(
+    ...invalidLogins.map(
+      (login) =>
+        new UsersAnalyzerWarning(
+          UsersAnalyzerWarning.INVALID_LOGIN,
+          `Le login ne doit comporter que des lettres : "${login}"`
+        )
+    )
+  );
+
+  warnings.push(
+    ...tooLongLogins.map(
+      (login) =>
+        new UsersAnalyzerWarning(
+          UsersAnalyzerWarning.INVALID_LOGIN,
+          `Le login est trop long (max ${LOGIN_MAX_LENGTH}) : "${login}"`
+        )
+    )
+  );
+
+  warnings.push(
+    ...tooClosesLogin.map(
+      (group) =>
+        new UsersAnalyzerWarning(
+          UsersAnalyzerWarning.TOO_CLOSE_LOGIN,
+          `Ces logins sont très proche : "${group.join('", "')}"`
+        )
+    )
+  );
 
   return warnings;
 }
 
 /**
- * Check if a login is valid
- * @param {string} login The login
- * @returns {UsersAnalyzerWarning[]} The warnings
+ * Filter logins to keep only invalid ones
+ * @param {any[]} logins The logins list
+ * @returns {any[]} The invalid logins
  */
-function isLoginValid(login) {
-  if (!isString(login)) {
-    return [
-      new UsersAnalyzerWarning(
-        UsersAnalyzerWarning.INVALID_LOGIN,
-        `Le login doit être une chaine de caractère : "${login}"`
-      ),
-    ];
-  }
-  const warnings = [];
+function getInvalidLogins(logins) {
+  return logins.filter((login) => !isString(login) || !/^[a-z]+$/i.test(login));
+}
 
-  if (login.length > LOGIN_MAX_LENGTH) {
-    warnings.push(
-      new UsersAnalyzerWarning(
-        UsersAnalyzerWarning.INVALID_LOGIN,
-        `Le login est trop long (max ${LOGIN_MAX_LENGTH}) : "${login}"`
-      )
-    );
-  }
-
-  if (/^[a-z]+$/i.test(login)) {
-    warnings.push(
-      new UsersAnalyzerWarning(
-        UsersAnalyzerWarning.INVALID_LOGIN,
-        `Le login ne doit contenir que des lettres : "${login}"`
-      )
-    );
-  }
+/**
+ * Filter logins to keep too long ones
+ * @param {any[]} logins The logins list
+ * @returns {any[]} The too long logins
+ */
+function getTooLongLogins(logins) {
+  return logins.filter((login) => isString(login) && login.length > LOGIN_MAX_LENGTH);
 }
 
 /**
@@ -56,6 +86,42 @@ function isLoginValid(login) {
  */
 function getDuplicates(values) {
   return [...new Set(values.filter((value, i) => values.slice(i + 1).includes(value)))];
+}
+
+/**
+ * Returns all groups of values that have a distance less than a given one
+ * @param {string[]} logins The login list
+ * @param {number} minDistance The maximum distance
+ * @returns {string[][]}
+ */
+function getTooCloseLogins(logins) {
+  logins = logins.slice();
+  const groups = [];
+
+  logins.forEach((value) => {
+    if (!isString(value)) {
+      return;
+    }
+    const group = logins.filter((other) => {
+      if (!isString(other)) {
+        return;
+      }
+      const distance = levenshtein(other, value);
+      return distance <= LOGIN_MAX_DISTANCE && distance > 0;
+    });
+
+    const existingGroup = groups.find((egroup) => egroup.some((e) => group.includes(e)));
+    if (existingGroup) {
+      existingGroup.push(...group, value);
+      return;
+    }
+
+    if (group.length > 0) {
+      groups.push([...group, value]);
+    }
+  });
+
+  return groups.map((group) => [...new Set(group)]);
 }
 
 const isString = (v) => typeof v === "string" || v instanceof String;
---
+++ server/test/users_importation/analyzer/usersAnalyzer.test.js
@@ -0,0 +1,52 @@
+import path from "path";
+
+import chai from "chai";
+
+import {
+  analyzeUsers,
+  UsersAnalyzerWarning,
+} from "../../../global/users_importation/usersAnalyzer.js";
+import { parseUsersFromCsv } from "../../../global/users_importation/usersParser.js";
+
+const { expect } = chai;
+const FILES_DIR = path.resolve("test", "users_importation", "analyzer", "files");
+
+const tests = [
+  {
+    file: "students.csv",
+    warnings: [
+      { code: UsersAnalyzerWarning.DUPLICATE_LOGIN, count: 0 },
+      { code: UsersAnalyzerWarning.INVALID_LOGIN, count: 0 },
+      { code: UsersAnalyzerWarning.TOO_CLOSE_LOGIN, count: 0 },
+      { code: UsersAnalyzerWarning.INVALID_ADMIN, count: 0 },
+    ],
+  },
+];
+
+const warningsCounter = (warnings, code) =>
+  warnings.reduce((count, warning) => count + (warning.code === code), 0);
+
+describe("Users analyzer", () => {
+  for (const test of tests) {
+    describe(`File ${test.file}`, () => {
+      let users;
+      before("Parse file", (done) => {
+        parseUsersFromCsv(path.resolve(FILES_DIR, test.file)).then((data) => {
+          users = JSON.parse(data);
+          done();
+        });
+      });
+
+      it("Expected warnings", (done) => {
+        const warnings = analyzeUsers(users);
+        console.log(warnings);
+        test.warnings.forEach((warning) =>
+          expect(warningsCounter(warnings, warning.code), "Type " + warning.code).equals(
+            warning.count
+          )
+        );
+        done();
+      });
+    });
+  }
+});
