25a812d57beb22e2a855b5ff5c6c18fcde9b471b
Francois Poguet <francoispoguet@gmail.com>
2021-02-22 12:07:53 +0100

    reset config before tests

--- server/db/updates/db_20210219_to_20210222.sql
+++ server/db/updates/db_20210219_to_20210222.sql
@@ -2,5 +2,5 @@ ALTER TABLE `server_informations`
     CHANGE `key` `key` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL; 
 
 UPDATE `server_informations`
-    SET `value` = "2021-02-19" WHERE `key` = "api_version";
+    SET `value` = "2021-02-22" WHERE `key` = "api_version";
     
--- server/test/required_data/config.sql
+++ server/test/required_data/config.sql
@@ -1,3 +1,3 @@
-INSERT INTO server_informations VALUES ("duel_questions_per_round",5);
-INSERT INTO server_informations VALUES ("duel_rounds_per_duel",5);
-INSERT INTO server_informations VALUES ("question_timer_duration",10);
+INSERT INTO server_informations VALUES ("config_duel_questions_per_round",5);
+INSERT INTO server_informations VALUES ("config_duel_rounds_per_duel",5);
+INSERT INTO server_informations VALUES ("config_question_timer_duration",10);
