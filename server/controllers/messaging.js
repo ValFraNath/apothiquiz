a6c78fc72eeb15824d64ac4a0a5e3147697a7dee
Valentin Perignon <valentin.perignon@orange.fr>
2021-02-27 12:19:20 +0100

    add request

--- server/controllers/messaging.js
+++ server/controllers/messaging.js
@@ -1,19 +1,57 @@
 import admin from "firebase-admin";
 
+import { queryPromise } from "../db/database";
 import saKey from "../files/serviceAccountKey.json";
+import HttpResponseWrapper from "../global/HttpResponseWrapper";
+import { addErrorTitle } from "../global/Logger";
 
 /* Initialize firebase */
 admin.initializeApp({
   credential: admin.credential.cert(saKey),
 });
 
-/* Users tokens */
-const subscribedUsers = {};
+/**
+ *
+ * @api {patch} /messaging/token/
+ * @apiName SaveUserToken
+ * @apiGroup Messaging
+ *
+ * @apiParam {string} messaging The messaging token
+ *
+ * @apiSuccess (200) {string} New messaging token
+ *
+ * @apiParamExample {object} Request-Example:
+ * {
+ *   "user": "nhoun",
+ *   "messagingToken": "cjFl5JjYiaUdFfxuKefa...iv9ViE6_m"
+ * }
+ *
+ *
+ * @apiUse ErrorServer
+ * @apiError (400) InvalidUser The user is not valid
+ * @apiError (400) InvalidToken The token is not valid
+ */
+export function updateToken(req, _res) {
+  const res = new HttpResponseWrapper(_res);
 
-export function sendNotificationToOneDevice(username, data) {
+  const { user, messagingToken } = req.body;
+
+  if (!user) {
+    return res.sendUsageError(400, "Missing user");
+  }
+  if (!messagingToken) {
+    return res.sendUsageError(400, "Missing messaging token");
+  }
+
+  saveMessagingTokenInDatabase()
+    .then((res) => console.log("RES -->", res))
+    .catch(res.sendServerError);
+}
+
+export function sendNotificationToOneDevice(targetToken, data) {
   const message = {
     data: data,
-    token: subscribedUsers[username],
+    token: targetToken,
   };
 
   admin
@@ -22,14 +60,15 @@ export function sendNotificationToOneDevice(username, data) {
     .catch((err) => console.error("Can't send notification to a single device", err));
 }
 
-export function sendNotificationToAllDevices(data) {
-  const message = {
-    data: data,
-    tokens: Object.values(subscribedUsers),
-  };
+// ***** INTERNAL FUNCTIONS *****
 
-  admin
-    .messaging()
-    .sendMulticast(message)
-    .catch((err) => console.error("Can't send notification to several devices", err));
+function saveMessagingTokenInDatabase(user, messagingToken) {
+  return new Promise((resolve, reject) => {
+    const sql = `UPDATE user
+                 SET us_messaging_token = ?
+                 WHERE us_login = ?`;
+    queryPromise(sql, [messagingToken, user])
+      .then((res) => resolve(res))
+      .catch((error) => reject(addErrorTitle(error, "Can't update messaging token", true)));
+  });
 }
