15ee3d3543bac8cdce725e99d6e95cf2904746f6
Francois Poguet <francoispoguet@gmail.com>
2021-02-28 16:04:20 +0100

    insert config in the update

--- server/db/updates/db_20210219_to_20210222.sql
+++ server/db/updates/db_20210219_to_20210222.sql
@@ -17,6 +17,10 @@ CREATE PROCEDURE `createDuel`(IN `player1` VARCHAR(32),
     SELECT @id as id; 
   END;
 
+INSERT INTO server_informations VALUES ("config_duel_questions_per_round",5);
+INSERT INTO server_informations VALUES ("config_duel_rounds_per_duel",5);
+INSERT INTO server_informations VALUES ("config_question_timer_duration",10);  
+
 UPDATE `server_informations`
     SET `value` = "2021-02-22" WHERE `key` = "api_version";
     
