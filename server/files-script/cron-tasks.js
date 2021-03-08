61c1cd4e97b798523feb8f85d6a687c9f0cc3c1c
Valentin Perignon <valentin.perignon.vp@gmail.com>
2021-03-08 11:32:45 +0100

    Update code

--- server/files-script/cron-tasks.js
+++ server/files-script/cron-tasks.js
@@ -22,7 +22,7 @@ const removeDuels = cron.schedule("00 01 00 * * *", function () {
  * Should be executed every 2 hour
  */
 const checkDuels = cron.schedule("*/10 * * * * *", function () {
-  const sql = `SELECT duel.du_id, us_login, re_last_time \
+  const sql = `SELECT duel.du_id, us_login, re_answers, re_last_time \
                FROM results, duel \
                WHERE results.du_id = duel.du_id \
                  AND duel.du_inProgress = 1;`;
@@ -38,25 +38,37 @@ const checkDuels = cron.schedule("*/10 * * * * *", function () {
         }
         duelsTime[data.du_id].push({
           user: data.us_login,
+          answers: data.re_answers,
           time: lastTime === null ? null : new Date(lastTime),
         });
       });
 
       Object.keys(duelsTime).forEach((key) => {
-        console.log(duelsTime[key]);
         const currentRound = duelsTime[key];
-        let indexLastPlayed = 0;
 
+        let indexLastPlayed = 0;
         if (currentRound[0].time === null || currentRound[1].time === null) {
           indexLastPlayed = currentRound[0].time ? 0 : 1;
         } else {
-          const lastTime = currentRound[0].time > currentRound[1].time ? 0 : 1;
+          indexLastPlayed = currentRound[0].time > currentRound[1].time ? 0 : 1;
         }
 
         if (diffDateInHour(new Date(), currentRound[indexLastPlayed].time) >= 24) {
-          console.log("Va perdre");
+          const resSample = JSON.parse(currentRound[indexLastPlayed].answers)[0];
+          const badResults = new Array(resSample.length).fill(-1);
+          const looser = currentRound[indexLastPlayed ^ 1];
+
+          const sql =
+            "UPDATE results \
+             SET re_answers = :answers, re_last_time = :time \
+             WHERE us_login = :login \
+               AND du_id = :id";
+          queryPromise(sql, {
+            answers: JSON.stringify([...looser.answers, badResults]),
+            time: formatDate(),
+            login: looser.user,
+          }).catch((err) => console.error("Can't update duel", err));
         }
-        console.log(diffDateInHour(new Date(), currentRound[indexLastPlayed].time));
       });
     })
     .catch((err) => console.error("Error: can't get in progress duels", err));
