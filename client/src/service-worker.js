3b0ef41d61feaf7bd9e4c1b7b8eaec2d9f15d0dd
Valentin Perignon <valentin.perignon.vp@gmail.com>
2021-03-27 19:45:55 +0100

    clean sw

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,11 +1,20 @@
+import { setCacheNameDetails } from "workbox-core";
 import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
 
 /* eslint-disable no-restricted-globals */
 
+/*
+ * Configure the workbox cache
+ */
+setCacheNameDetails({
+  prefix: "guacamole",
+  suffix: "v1",
+});
+
 /**
- * Install the service worker
+ * Install event
  */
 self.addEventListener("install", (e) => {
   console.info("[Service Worker] Install");
@@ -17,30 +26,15 @@ self.addEventListener("install", (e) => {
 precacheAndRoute(self.__WB_MANIFEST);
 
 /**
- * Activate the service worker
- * Older versions of the cache (with a different cache name than the current one) are deleted.
+ * Activate event
  */
 self.addEventListener("activate", (e) => {
   console.info("[Service Worker] Activate");
 });
 
-function matchRegularRoute({ url }) {
-  return !url.pathname.startsWith("/api");
-}
-
-registerRoute(matchRegularRoute, new StaleWhileRevalidate());
-
 /**
  * Fetch resources
- * If it is a call to the API, it is performed as usual
- * Otherwise, either the resource is cached and returned to the user, or the resource is not
- * cached, it is cached and returned.
+ * TODO: Blabla
  */
-
-self.addEventListener("message", (e) => {
-  if (e.data.type && e.data.type === "SKIP_WAITING") {
-    self
-      .skipWaiting()
-      .catch((err) => console.error("Error: can't skip waiting service-worker", err));
-  }
-});
+const matchRouteToCache = ({ url }) => !url.pathname.startsWith("/api");
+registerRoute(matchRouteToCache, new StaleWhileRevalidate());
