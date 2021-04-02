288dbdd4af51622d22f2c226cb7727e7a9d8e660
Valentin Perignon <valentin.perignon.vp@gmail.com>
2021-04-02 10:55:56 +0200

    first fix

--- server/files-script/cron-tasks.js
+++ server/files-script/cron-tasks.js
@@ -31,68 +31,66 @@ const removeDuelsTask = cron.schedule(
   }
 );
 
-/**
- * Make lose the current round for players who have not played 24 hours after the start
- * Should be executed every 3 hours
- */
-const checkDuelsTask = cron.schedule(
-  "* * */3 * * *",
-  async function () {
-    const sql = `SELECT duel.du_id, du_content, us_login, re_answers, re_last_time \
+export async function t() {
+  const sql = `SELECT duel.du_id, du_content, us_login, re_answers, re_last_time \
                  FROM results, duel \
                  WHERE results.du_id = duel.du_id \
                    AND duel.du_inProgress = 1;`;
 
-    try {
-      const listOfDuels = {};
+  try {
+    const listOfDuels = {};
 
-      const res = await queryPromise(sql);
-      res.forEach((element) => {
-        if (!listOfDuels[element.du_id]) listOfDuels[element.du_id] = [];
-        listOfDuels[element.du_id].push({
-          login: element.us_login,
-          content: element.du_content,
-          answers: element.re_answers,
-          lastTime: element.re_last_time,
-        });
+    const res = await queryPromise(sql);
+    res.forEach((element) => {
+      if (!listOfDuels[element.du_id]) listOfDuels[element.du_id] = [];
+      listOfDuels[element.du_id].push({
+        login: element.us_login,
+        content: JSON.parse(element.du_content),
+        answers: JSON.parse(element.re_answers),
+        lastTime: element.re_last_time,
       });
+    });
 
-      await Promise.all(
-        Object.keys(listOfDuels).map(async (duelID) => {
-          const currentDuel = listOfDuels[duelID];
+    await Promise.all(
+      Object.keys(listOfDuels).map(async (duelID) => {
+        const currentDuel = listOfDuels[duelID];
 
-          let needToPlayIndex = 0;
-          if (currentDuel[0].answers.length === currentDuel[1].answers.length) {
-            needToPlayIndex =
-              new Date(currentDuel[0].lastTime) > new Date(currentDuel[1].lastTime) ? 1 : 0;
-          } else {
-            needToPlayIndex = currentDuel[0].answers.length > currentDuel[1].answers.length ? 1 : 0;
-          }
+        let needToPlayIndex = 0;
+        if (currentDuel[0].answers.length === currentDuel[1].answers.length) {
+          needToPlayIndex =
+            new Date(currentDuel[0].lastTime) > new Date(currentDuel[1].lastTime) ? 1 : 0;
+        } else {
+          needToPlayIndex = currentDuel[0].answers.length > currentDuel[1].answers.length ? 1 : 0;
+        }
 
-          const hoursSinceLastPlay = diffDateInHour(
-            new Date(),
-            new Date(currentDuel[needToPlayIndex].lastTime)
+        const hoursSinceLastPlay = diffDateInHour(
+          new Date(),
+          new Date(currentDuel[needToPlayIndex].lastTime)
+        );
+        if (Math.ceil(hoursSinceLastPlay) >= 24) {
+          const numberOfAnswers = currentDuel[needToPlayIndex].content[0].length;
+          const fakeAnswers = new Array(numberOfAnswers).fill(-1);
+
+          const updatedDuels = await Duels.insertResultInDatabase(
+            duelID,
+            currentDuel[needToPlayIndex].login,
+            fakeAnswers
           );
-          if (Math.ceil(hoursSinceLastPlay) >= 24) {
-            const numberOfAnswers = JSON.parse(currentDuel[needToPlayIndex].content)[0].length;
-            const fakeAnswers = new Array(numberOfAnswers).fill(-1);
-
-            const updatedDuels = await Duels.insertResultInDatabase(
-              duelID,
-              currentDuel[needToPlayIndex].login,
-              fakeAnswers
-            );
-            await Duels.updateDuelState(updatedDuels, currentDuel[needToPlayIndex].login);
-          }
-        })
-      );
-    } catch (err) {
-      Logger.error(err, "Can't check duels");
-    }
-  },
-  {
-    scheduled: false,
+          await Duels.updateDuelState(updatedDuels, currentDuel[needToPlayIndex].login);
+        }
+      })
+    );
+  } catch (err) {
+    Logger.error(err, "Can't check duels");
   }
-);
+}
+
+/**
+ * Make lose the current round for players who have not played 24 hours after the start
+ * Should be executed every 3 hours
+ */
+const checkDuelsTask = cron.schedule("* * */3 * * *", t, {
+  scheduled: false,
+});
 
 export { removeDuelsTask, checkDuelsTask };
