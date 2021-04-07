8e098ff3a785dd4184477614130dcde80fff9b57
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-04-07 07:37:14 +0200

    Remove tests from pre-commit hook, the CI will take care of this

--- githooks/pre-commit.sh
+++ githooks/pre-commit.sh
@@ -2,16 +2,16 @@
 
 if [ "$(git diff --name-only HEAD | grep "^server/")" != "" ]; then
     cd "$(git rev-parse --show-toplevel)/server" || exit 1
+    echo "Linting and formatting server files"
     npm run format:write || exit 1
     npm run lint:fix -- --max-warnings 0 || exit 1
-    npm run test || exit 1
 fi
 
 if [ "$(git diff --name-only HEAD | grep "^client/")" != "" ]; then
     cd "$(git rev-parse --show-toplevel)/client" || exit 1
+    echo "Linting and formatting client files"
     npm run format:write || exit 1
     npm run lint:fix -- --max-warnings 0 || exit 1
-    npm run test -- --watchAll=false --onlyChanged || exit 1
 fi
 
 cd "$(git rev-parse --show-toplevel)" || exit 1
