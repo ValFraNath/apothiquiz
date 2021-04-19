54e60c6019b3dca62f79ed41e4978908e3eba6c3
Erosiinho <erosalberola1@gmail.com>
2021-04-19 22:40:30 +0200

    remove {}

--- client/src/pages/Train.jsx
+++ client/src/pages/Train.jsx
@@ -69,8 +69,7 @@ const IntroductionView = ({ onClick }) => {
         </select>
       </div>
       <ButtonDefault onClick={() => onClick(system, difficulty)}>
-        {" "}
-        Lancer l'entraînement{" "}
+        Lancer l'entraînement
       </ButtonDefault>
     </>
   );
