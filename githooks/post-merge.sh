98fc5748f22e66d63fb2a94793a181154c8967f3
Nathanaël Houn <contact@nathanaelhoun.fr>
2023-02-17 17:09:44 +0100

    chore: updated githooks

--- githooks/post-merge.sh
+++ githooks/post-merge.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+docker compose up node --detach
+
 function changed() {
     git diff --name-only 'HEAD@{1}' HEAD | grep "^$1" >/dev/null 2>&1
 }
 
-cd "$(git rev-parse --show-toplevel)/server" || exit 1
 if changed 'server/package-lock.json'; then
     echo "📦 server/package-lock.json changed. Running npm install"
-    npm install || exit 1
+    docker compose exec --workdir "/app/server" --no-TTY node npm install  || exit 1
 fi
 
-cd "$(git rev-parse --show-toplevel)/client" || exit 1
 if changed 'client/package-lock.json'; then
     echo "📦 client/package-lock.json changed. Running npm install"
-    npm install || exit 1
+    docker compose exec --workdir "/app/client" --no-TTY node npm install  || exit 1
 fi
 
 exit 0
